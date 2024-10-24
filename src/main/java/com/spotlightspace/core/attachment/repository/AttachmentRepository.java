a0d42277fce5c3bd518cb0603ce98db7d0d7ef86
taeyoung kang <kty1467@naver.com>
2024-10-24 17:23:36 +0900

    feat : 이벤트 수정, 삭제, 첨부파일 단일 생성, 연관 삭제 등등 로직 작성 중

--- src/main/java/com/spotlightspace/core/attachment/repository/AttachmentRepository.java
+++ src/main/java/com/spotlightspace/core/attachment/repository/AttachmentRepository.java
@@ -1,8 +1,26 @@
 package com.spotlightspace.core.attachment.repository;
 
+import com.spotlightspace.common.entity.TableRole;
+import com.spotlightspace.common.exception.ApplicationException;
 import com.spotlightspace.core.attachment.domain.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+import static com.spotlightspace.common.exception.ErrorCode.ATTACHMENT_NOT_FOUND;
+
 public interface AttachmentRepository extends JpaRepository<Attachment, Long>, AttachmentQueryRepository {
 
+    Attachment findByTableRoleAndTargetId(TableRole tableRole, Long userId);
+    Optional<Attachment> findByTargetIdAndTableRole(Long targetId, TableRole tableRole);
+
+    default Attachment findByTargetIdAndTableRoleOrElseThrow(Long targetId, TableRole tableRole) {
+        return findByTargetIdAndTableRole(targetId, tableRole)
+                .orElseThrow(() -> new ApplicationException(ATTACHMENT_NOT_FOUND));
+    }
+
+    default Attachment findByIdOrElseThrow(long id) {
+        return findById(id)
+                .orElseThrow(() -> new ApplicationException(ATTACHMENT_NOT_FOUND));
+    }
 }
--- src/main/java/com/spotlightspace/core/event/service/EventService.java
+++ src/main/java/com/spotlightspace/core/event/service/EventService.java
@@ -7,6 +7,8 @@ import com.spotlightspace.core.attachment.service.AttachmentService;
 import com.spotlightspace.core.event.domain.Event;
 import com.spotlightspace.core.event.dto.AddEventRequestDto;
 import com.spotlightspace.core.event.dto.AddEventResponseDto;
+import com.spotlightspace.core.event.dto.UpdateEventRequestDto;
+import com.spotlightspace.core.event.dto.UpdateEventResponseDto;
 import com.spotlightspace.core.event.repository.EventRepository;
 import com.spotlightspace.core.user.domain.User;
 import com.spotlightspace.core.user.domain.UserRole;
@@ -45,6 +47,57 @@ public class EventService {
         return AddEventResponseDto.from(event);
     }
 
+    @Transactional
+    public UpdateEventResponseDto updateEvent(UpdateEventRequestDto requestDto, AuthUser authUser, Long id) {
+        // 이벤트 존재 유무 검사
+        Event event = checkEventExist(id);
+        // 이벤트를 작성한 아티스트인가 검사
+        checkEventAndUser(event, authUser);
+
+        // 수정 로직
+        if (requestDto.getTitle() != null) {
+            event.changeTitle(requestDto.getTitle());
+        }
+        if (requestDto.getContent() != null) {
+            event.changeContent(requestDto.getContent());
+        }
+        if (requestDto.getStartAt() != null) {
+            event.changeStartAt(requestDto.getStartAt());
+        }
+        if (requestDto.getEndAt() != null) {
+            event.changeEndAt(requestDto.getEndAt());
+        }
+        if (requestDto.getMaxPeople() != null) {
+            // 변경하려는 maxPeople값이 이미 결제한 사람 언더일 때 exception 처리 해야함
+            event.changeMaxPeople(requestDto.getMaxPeople());
+        }
+        if (requestDto.getPrice() != null) {
+            event.changePrice(requestDto.getPrice());
+        }
+        if (requestDto.getCategory() != null) {
+            event.changeCategory(requestDto.getCategory());
+        }
+        if (requestDto.getRecruitmentStartAt() != null) {
+            event.changeRecruitmentStartAt(requestDto.getRecruitmentStartAt());
+        }
+        if (requestDto.getRecruitmentFinishAt() != null) {
+            event.changeRecruitmentFinishAt(requestDto.getRecruitmentFinishAt());
+        }
+        eventRepository.save(event);
+        return UpdateEventResponseDto.from(event);
+    }
+
+    @Transactional
+    public void deleteEvent(Long id, AuthUser authUser) {
+        // 이벤트 존재 유무 검사
+        Event event = checkEventExist(id);
+        // 이벤트를 작성한 아티스트인가 검사
+        checkEventAndUser(event, authUser);
+        // 삭제 진행 시 결제한 사람 (포인트, 쿠폰)환불처리 + 관련 이미지 삭제
+        attachmentService.deleteAttachmentWithOtherTable(event.getId(), TableRole.EVENT);
+        event.deleteEvent();
+    }
+
     // 유저 존재 확인
     private User checkUserExist(Long id) {
         return userRepository.findByIdOrElseThrow(id);
@@ -54,6 +107,18 @@ public class EventService {
     private void validateUserRole(UserRole role) {
         if (role != UserRole.ROLE_ARTIST) {
             throw new ApplicationException(USER_NOT_ARTIST);
+        }
+    }
+
+    // 이벤트 존재 확인
+    private Event checkEventExist(Long id) {
+        return eventRepository.findByIdOrElseThrow(id);
+    }
+
+    // 이벤트를 작성한 사람인가 확인
+    private void checkEventAndUser(Event event, AuthUser authUser) {
+        if(!event.getUser().getId().equals(authUser.getUserId())) {
+            throw new ApplicationException(USER_NOT_ACCESS_EVENT);
         }
     }
 }
