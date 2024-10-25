928c807a8c87bed7b71a157a71bfc46629211459
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-10-26 03:09:32 +0900

    feat : 로그아웃 구현
    - 로그아웃을 구현하였습니다
    - redis에 blackList를 구현하여 현재 user의 jwt access토큰을 입력합니다
    - jwtFilter에서 현재 토큰이 blackList에 있는지 검증을하는 메서드를 추가하였습니다.

--- src/main/java/com/spotlightspace/core/user/service/UserService.java
+++ src/main/java/com/spotlightspace/core/user/service/UserService.java
@@ -1,11 +1,14 @@
 package com.spotlightspace.core.user.service;
 
+import static com.spotlightspace.common.constant.JwtConstant.TOKEN_ACCESS_TIME;
 import static com.spotlightspace.common.exception.ErrorCode.FORBIDDEN_USER;
 import static com.spotlightspace.common.exception.ErrorCode.USER_NOT_FOUND;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 import com.spotlightspace.common.annotation.AuthUser;
 import com.spotlightspace.common.entity.TableRole;
 import com.spotlightspace.common.exception.ApplicationException;
+import com.spotlightspace.config.JwtUtil;
 import com.spotlightspace.core.attachment.service.AttachmentService;
 import com.spotlightspace.core.user.domain.User;
 import com.spotlightspace.core.user.dto.request.UpdateUserRequestDto;
@@ -13,9 +16,14 @@ import com.spotlightspace.core.user.dto.response.GetCouponResponseDto;
 import com.spotlightspace.core.user.dto.response.GetUserResponseDto;
 import com.spotlightspace.core.user.repository.UserRepository;
 import com.spotlightspace.core.usercoupon.service.UserCouponService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +38,8 @@ public class UserService {
     private final AttachmentService attachmentService;
     private final PasswordEncoder passwordEncoder;
     private final UserCouponService userCouponService;
+    private final JwtUtil jwtUtil;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public void updateUser(Long userId, AuthUser authUser, UpdateUserRequestDto updateUserRequestDto,
             MultipartFile file)
@@ -97,4 +107,25 @@ public class UserService {
 
         return userCouponService.getUserCouponByUserId(userId);
     }
+
+    //재발급 방지를 위해 redis의 리프레시 토큰 삭제
+    // 액세스 토큰 레디스에올리기
+    // 쿠키 무효화
+    public void logout(Long userId, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        //쿠키에 있는 리프레시토큰 무효화
+        Cookie refreshTokenCookie = new Cookie("RefreshToken", null);
+        refreshTokenCookie.setMaxAge(0);
+        refreshTokenCookie.setPath("/");
+        httpServletResponse.addCookie(refreshTokenCookie);
+
+        String accessToken = httpServletRequest.getHeader(AUTHORIZATION);
+        String key = "user:blacklist:id:" + userId;
+        //redis에 해당하는 리프레시 토큰 삭제
+        redisTemplate.delete("user:refresh:id:" + userId);
+
+        //액세스 토큰 레디스에 블랙리스트로 올리기
+        redisTemplate.opsForValue()
+                .set(key, accessToken, TOKEN_ACCESS_TIME, TimeUnit.MILLISECONDS);
+    }
+
 }
