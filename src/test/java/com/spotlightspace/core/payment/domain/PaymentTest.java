ebd7955f7ff868237d8026f9ee46df61b72eaadd
bronbe <bronsta111@gmail.com>
2024-10-25 22:19:07 +0900

    test: Payment 테스트 코드 작성

---
+++ src/test/java/com/spotlightspace/core/payment/domain/PaymentTest.java
@@ -0,0 +1,72 @@
+package com.spotlightspace.core.payment.domain;
+
+import static com.spotlightspace.core.payment.domain.PaymentStatus.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.spotlightspace.core.auth.dto.SignupUserRequestDto;
+import com.spotlightspace.core.event.domain.Event;
+import com.spotlightspace.core.event.domain.EventCategory;
+import com.spotlightspace.core.event.dto.AddEventRequestDto;
+import com.spotlightspace.core.user.domain.User;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PaymentTest {
+
+    @Test
+    @DisplayName("approve 호출 시 Payment 상태가 APPROVED 가 된다.")
+    void approve() {
+        // given
+        User user = createUser();
+        Event event = createEvent(user);
+        Payment payment = Payment.create("cid", event, user, 10_000);
+
+        // when
+        payment.approve();
+
+        // then
+        assertThat(payment.getStatus()).isEqualTo(APPROVED);
+    }
+
+    @Test
+    @DisplayName("Payment 생성 시 초기 상태는 READY 이다.")
+    void create() {
+        // given
+        User user = createUser();
+        Event event = createEvent(user);
+        Payment payment = Payment.create("cid", event, user, 10_000);
+
+        // when & then
+        assertThat(payment.getStatus()).isEqualTo(READY);
+    }
+
+
+    private User createUser() {
+        SignupUserRequestDto requestDto = new SignupUserRequestDto(
+                "test142@email.com",
+                "rawPassword",
+                "nickname",
+                "role_user",
+                "1998-12-12",
+                "010-1234-1234"
+        );
+        return User.of("encryptPassword", requestDto);
+    }
+
+    private Event createEvent(User user) {
+        AddEventRequestDto addEventRequestDto = new AddEventRequestDto(
+                "title",
+                "content",
+                "서울",
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                10,
+                10_000,
+                EventCategory.ART,
+                LocalDateTime.of(2024,10,10, 10, 10),
+                LocalDateTime.of(2024, 10, 12, 10, 10)
+        );
+        return Event.of(addEventRequestDto, user);
+    }
+}
