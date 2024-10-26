f2982847e822db1e9558eef49dab45f23f11e0ed
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-10-26 22:29:39 +0900

    refactor : 에러코드 정렬
    - 에러코드를 오름차순으로 정렬하여, 가독성을 높였습니다.

--- src/main/java/com/spotlightspace/common/exception/ErrorCode.java
+++ src/main/java/com/spotlightspace/common/exception/ErrorCode.java
@@ -1,6 +1,9 @@
 package com.spotlightspace.common.exception;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -8,11 +11,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    USER_NOT_FOUND(NOT_FOUND, "존재하지 않는 유저입니다."),
-    INVALID_PASSWORD_OR_EMAIL(NOT_FOUND, "이메일 또는 패스워드가 일치하지 않습니다"),
     EMAIL_DUPLICATED(CONFLICT, "이미 존재하는 이메일입니다."),
     FORBIDDEN_USER(FORBIDDEN, "권한이 없습니다."),
-    INVALID_REFRESH_TOKEN(FORBIDDEN,"리프레시토큰이 없습니다"),
+    INVALID_REFRESH_TOKEN(FORBIDDEN, "리프레시토큰이 없습니다"),
+    USER_NOT_FOUND(NOT_FOUND, "존재하지 않는 유저입니다."),
+    INVALID_PASSWORD_OR_EMAIL(NOT_FOUND, "이메일 또는 패스워드가 일치하지 않습니다"),
 
     ADMIN_NOT_FOUND(NOT_FOUND, "존재하지 않는 관리자입니다."),
 
@@ -37,11 +40,11 @@ public enum ErrorCode {
 
     REVIEW_NOT_FOUND(NOT_FOUND, "존재하지 않는 리뷰입니다."),
 
-    UNAUTHORIZED (NOT_FOUND, "티켓을 결제한 사용자만 리뷰를 작성 할 수 있습니다."),
+    UNAUTHORIZED(NOT_FOUND, "티켓을 결제한 사용자만 리뷰를 작성 할 수 있습니다."),
 
     WRONG_CATEGORY_NAME(BAD_REQUEST, "유효하지 않는 카테고리 입니다."),
 
-    USER_NOT_ARTIST(FORBIDDEN,"일반 유저는 해당 작업을 수행할 수 없습니다."),
+    USER_NOT_ARTIST(FORBIDDEN, "일반 유저는 해당 작업을 수행할 수 없습니다."),
 
     TABLE_NOT_FOUND(NOT_FOUND, "테이블이 존재하지 않습니다."),
 
