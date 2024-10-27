afad9ab25178199c824d61779b2028523109abc8
taeyoung kang <kty1467@naver.com>
2024-10-27 19:17:05 +0900

    refactor : 포인트 적립 로직 변경

--- src/main/java/com/spotlightspace/core/point/dto/CreatePointRequestDto.java
+++ src/main/java/com/spotlightspace/core/point/dto/CreatePointRequestDto.java
@@ -1,10 +1,13 @@
 package com.spotlightspace.core.point.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreatePointRequestDto {
     private int price;
 }
