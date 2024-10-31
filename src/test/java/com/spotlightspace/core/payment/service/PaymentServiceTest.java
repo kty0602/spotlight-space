60d275e0ef1011778b9fd9f1acfafbed9af4b715
bronbe <bronsta111@gmail.com>
2024-10-31 17:23:58 +0900

    test: 결제 동시성 테스트 작성

---
+++ src/test/java/com/spotlightspace/core/payment/service/PaymentServiceTest.java
@@ -0,0 +1,123 @@
+package com.spotlightspace.core.payment.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.anyInt;
+import static org.mockito.BDDMockito.anyLong;
+import static org.mockito.BDDMockito.anyString;
+import static org.mockito.BDDMockito.given;
+
+import com.spotlightspace.core.data.UserTestData;
+import com.spotlightspace.core.event.domain.Event;
+import com.spotlightspace.core.event.domain.EventCategory;
+import com.spotlightspace.core.event.dto.request.CreateEventRequestDto;
+import com.spotlightspace.core.event.repository.EventRepository;
+import com.spotlightspace.core.eventticketstock.domain.EventTicketStock;
+import com.spotlightspace.core.eventticketstock.repository.EventTicketStockRepository;
+import com.spotlightspace.core.payment.domain.Payment;
+import com.spotlightspace.core.payment.dto.response.ReadyPaymentResponseDto;
+import com.spotlightspace.core.payment.repository.PaymentRepository;
+import com.spotlightspace.core.user.domain.User;
+import com.spotlightspace.core.user.repository.UserRepository;
+import com.spotlightspace.integration.kakaopay.KakaopayApi;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@Slf4j
+@SpringBootTest
+class PaymentServiceTest {
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    EventTicketStockRepository eventTicketStockRepository;
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    @MockBean
+    KakaopayApi kakaopayApi;
+
+    @AfterEach
+    void tearDown() {
+        paymentRepository.deleteAllInBatch();
+        eventTicketStockRepository.deleteAllInBatch();
+        eventRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("결제 동시성 테스트 - Pessimistic Lock")
+    void pessimisticLock() throws InterruptedException {
+        // given
+        User user = userRepository.save(UserTestData.testUser());
+        CreateEventRequestDto createEventRequestDto = getCreateEventRequestDtoWithMaxPeople(10);
+        Event event = eventRepository.save(Event.of(createEventRequestDto, user));
+        EventTicketStock eventTicketStock = eventTicketStockRepository.save(EventTicketStock.create(event));
+
+        given(kakaopayApi.readyPayment(anyString(), anyLong(), anyLong(), anyString(), anyLong(), anyInt()))
+                .willReturn(createReadyPaymentResponseDto());
+
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    paymentService.readyPayment(user.getId(), event.getId(), null, null);
+                } catch (Exception e) {
+                    System.err.println("결제 실패: " + e.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        // then
+        List<Payment> payments = paymentRepository.findAll();
+        EventTicketStock foundEventTicketStock = eventTicketStockRepository.findById(eventTicketStock.getId()).get();
+        assertThat(payments).hasSize(10);
+        assertThat(foundEventTicketStock.getStock()).isEqualTo(0);
+    }
+
+    ReadyPaymentResponseDto createReadyPaymentResponseDto() {
+        return new ReadyPaymentResponseDto(
+                "tid",
+                "url",
+                "url",
+                "url",
+                "url",
+                "url",
+                LocalDateTime.now()
+        );
+    }
+
+    CreateEventRequestDto getCreateEventRequestDtoWithMaxPeople(int maxPeople) {
+        return CreateEventRequestDto.of("title", "content", "location",
+                LocalDateTime.of(2024, 12, 12, 12, 0),
+                LocalDateTime.of(2024, 12, 12, 14, 0), maxPeople, 10_000,
+                EventCategory.ART, LocalDateTime.of(2024, 10, 10, 0, 0),
+                LocalDateTime.of(2024, 12, 10, 0, 0));
+    }
+
+}
